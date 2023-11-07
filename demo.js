fbf19d53b96ce483cc85cb53dc308f911730cd46
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-07 23:04:24 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,5 +1,85 @@
 console.log("Welcome to Java Script")
-let a = 55
+let t = 55
 console.log(a);
-a = 'Jitu'
-console.log(a);
+t = 'Jitu'
+console.log(t);
+//...........................................
+
+// 1. Ways to print in JavaScript.
+// console.log("Jitu");
+// alert("Hi...");
+// document.write("This is document write...")
+
+// 2. JavaScript console API
+// console.log("Hello World");
+// console.warn("This is warning...");
+// console.error("This is an Error.");
+
+// 3. JavaScript Variables.
+// What are variable? - Containers to store data value.
+var number1 = 34;
+var number2 = 56;
+// console.log(number1 + number2);
+
+// 4. Data types in JavaScript.
+// A. Numbers
+var num1 = 123;
+var num2 = 12.30;
+
+// B. String
+var str1 = "This is a string.";
+var str2 = 'This is also a string.';
+
+// C. Objects
+var marks = {
+    Ajay: 45,
+    Anil: 60,
+    Vinay: 65,
+    Jitu: 99.977
+}
+// console.log(marks);
+
+// D. Booleans
+var a = true;
+var b = false;
+// console.log(a, b);
+
+// var und = undefined;
+var und;
+// console.log(und);
+
+var n = null;
+// console.log(n);
+/*
+At a very high level, there are two types of data tpes in JavaScript.
+1. Primitive data types: undefined, null, number, string, boolean, symbol.
+2. Refrence data types: arrays and Objects.
+*/
+var arr = [1, 2, 'Jitu', 4, 5]
+// console.log(arr);
+
+// OPERATORS IN JAVASCRIPT.
+// Airthmetic operators.
+var a = 100;
+var b = 10;
+console.log('The value of a+b is', a+b);
+console.log('The value of a-b is', a-b);
+console.log('The value of a*b is', a*b);
+console.log('The value of a/b is', a/b);
+
+// Assignment Operators.
+var c = b;
+// c += 2;
+// c -= 2;
+// c *= 2;
+// c /= 2;
+// console.log(c);
+
+//Comparison Operators.
+var x = 55;
+var y = 66;
+console.log(x == y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x > y);
+console.log(x < y);
