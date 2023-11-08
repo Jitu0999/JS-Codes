092c8570233251aa1318750e7e8720fc51ebaa86
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-08 21:18:18 +0530

    Create 01_Datatype.js

---
+++ 01_Datatype.js
@@ -0,0 +1,11 @@
+// JavaScript Variables.
+// What are variable? - Containers to store data value.
+var number1 = 34;
+var number2 = 56;
+ console.log(number1 + number2);
+
+//Data types in JavaScript.
+// A. Numbers
+var num1 = 123;
+var num2 = 12.30;
+console.log(num1 + num2);
