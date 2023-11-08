5ca6e3214b97c1b4d3a0a3109bfb2191e60e15b0
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-08 22:08:24 +0530

    Update 01_Datatype.js

--- 01_Datatype.js
+++ 01_Datatype.js
@@ -7,18 +7,39 @@ var a = 'Jitu';
 let b = null
 var c = 55
 var d = undefined
+const author = 'Jitu' // cannot be change.
 {
     let b = 'this'
-    console.log(b)
+    //console.log(b)
 }
-console.log(b)
+//console.log(b)
 
 
 
 
- /******************************/
-//Data types in JavaScript.
+ /***************  DATA TYPES ********************/
+/*Data types in JavaScript. 7 Premitive data type
+Null, Number, Symbol, String, Boolean, Bigint, Undefined
+*/
+
+let i = null;
+let j = 555
+let k = true; // can also be false
+let l = 'Jitu'
+let m = Symbol('I am a nice symbol')
+let n = BigInt('444')
+let o = undefined
+//console.log(i, j, k, l, m, n, o)
+console.log(typeof i)
+console.log(typeof j)
+console.log(typeof k)
+console.log(typeof l)
+console.log(typeof m)
+console.log(typeof n)
+console.log(typeof o)
+
+
 // A. Numbers
 var num1 = 123;
 var num2 = 12.30;
-console.log(num1 + num2);
+//console.log(num1 + num2);
