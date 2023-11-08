376157dfa9d4e5a50fad5fe5b3e2db53c49557ea
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-08 21:39:03 +0530

    Update 01_Datatype.js

--- 01_Datatype.js
+++ 01_Datatype.js
@@ -2,8 +2,21 @@
 // What are variable? - Containers to store data value.
 var number1 = 34;
 var number2 = 56;
- console.log(number1 + number2);
+//console.log(number1 + number2);
+var a = 'Jitu';
+let b = null
+var c = 55
+var d = undefined
+{
+    let b = 'this'
+    console.log(b)
+}
+console.log(b)
 
+
+
+
+ /******************************/
 //Data types in JavaScript.
 // A. Numbers
 var num1 = 123;
