8c73e003850b6c9c3b2cadf831904480253de153
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-10 00:42:54 +0530

    Update PracticeSet1.js

--- PracticeSet1.js
+++ PracticeSet1.js
@@ -19,10 +19,32 @@ const x = {
     section: 1,
     isPrincipal: false
 }
-x = 55
-// TypeError: Assignment to constant variable. at Object.<anonymous>
+//x = 55 // TypeError: Assignment to constant variable. at Object.<anonymous>
 
 /*
 Q4. Try to add a new key to the const object in pbrblem 3 where you add to do it ?
 */
+const y = {
+    name: 'Jitu',
+    section: 1,
+    isPrincipal: false
+}
+y['friend'] = 'Naresh'
+y['name'] = 'Dinesh'
+console.log(y)
 
+/*
+Q5. Write a JS program to create a word-meaning dictionary to 5 words ?
+*/
+const dict = {
+    appreciate: 'to understand the value of somebody/something',
+    thank: 'to tell somebody that you are grateful',
+    beautiful: 'very pretty or attractive',
+    technology: 'the scientific knowledge',
+    intelligence: 'the ability to understand, learn and think'
+}
+console.log(dict.appreciate);
+console.log(dict.thank);
+console.log(dict.beautiful);
+console.log(dict.technology);
+console.log(dict.intelligence);
