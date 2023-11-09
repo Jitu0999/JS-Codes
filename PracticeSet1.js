0a2ebff360bbcbecddd80a698ca79cf7cd3513d5
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-09 23:37:56 +0530

    Create PracticeSet1.js

---
+++ PracticeSet1.js
@@ -0,0 +1,12 @@
+/*
+Q1. Create a variable of type string and try to add a number to it ?
+*/
+let a = 'Jitu'
+let b = 5
+console.log(a+b) // Jitu5
+
+
+/*
+Q2. Use typeof operator to find the datatype of the string in last question ?
+*/
+
