35a6d4f7f839a858b290f9b84b37c1828cf333d9
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-10 23:33:46 +0530

    Create 02_Operators.js

---
+++ 02_Operators.js
@@ -0,0 +1,25 @@
+// OPERATORS IN JAVASCRIPT.
+// Airthmetic operators.
+var a = 100;
+var b = 10;
+console.log('The value of a+b is', a+b);
+console.log('The value of a-b is', a-b);
+console.log('The value of a*b is', a*b);
+console.log('The value of a/b is', a/b);
+
+// Assignment Operators.
+var c = b;
+// c += 2;
+// c -= 2;
+// c *= 2;
+// c /= 2;
+ console.log(c);
+
+//Comparison Operators.
+var x = 55;
+var y = 66;
+//console.log(x == y);
+//console.log(x >= y);
+//console.log(x <= y);
+//console.log(x > y);
+//console.log(x < y);
