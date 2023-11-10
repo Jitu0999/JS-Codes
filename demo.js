d04459b7e513b9f2fbcaa2e1c2ea198b3fedd7d7
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-10 23:33:31 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -57,29 +57,3 @@ At a very high level, there are two types of data tpes in JavaScript.
 */
 var arr = [1, 2, 'Jitu', 4, 5]
 // console.log(arr);
-
-// OPERATORS IN JAVASCRIPT.
-// Airthmetic operators.
-var a = 100;
-var b = 10;
-console.log('The value of a+b is', a+b);
-console.log('The value of a-b is', a-b);
-console.log('The value of a*b is', a*b);
-console.log('The value of a/b is', a/b);
-
-// Assignment Operators.
-var c = b;
-// c += 2;
-// c -= 2;
-// c *= 2;
-// c /= 2;
- console.log(c);
-
-//Comparison Operators.
-var x = 55;
-var y = 66;
-//console.log(x == y);
-//console.log(x >= y);
-//console.log(x <= y);
-//console.log(x > y);
-//console.log(x < y);
