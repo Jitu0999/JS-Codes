19658da47a422b7c1d8608bf8905e404ce9a561b
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-17 22:54:16 +0530

    Update 02_Operators.js

--- 02_Operators.js
+++ 02_Operators.js
@@ -1,52 +1,54 @@
 /* 
 ************ OPERATORS IN JAVASCRIPT. **************
 */ 
-//Airthmetic operators.
+//---------Airthmetic operators.
 let i = 10;
 let j = 4;
-console.log('i+j =', i+j)
-console.log('i-j =', i-j)
-console.log('i/j =', i/j)
-console.log('i % j =', i%j)
-console.log('i*j =', i*j)
-console.log('i**j =', i**j)
-console.log('i++ =', i++)
-console.log('++i =', ++i)
-console.log('i-- =', i--)
-console.log('i=', i)
+//console.log('i+j =', i+j)
+// console.log('i-j =', i-j)
+// console.log('i/j =', i/j)
+// console.log('i % j =', i%j)
+// console.log('i*j =', i*j)
+// console.log('i**j =', i**j)
+// console.log('i++ =', i++)
+// console.log('++i =', ++i)
+// console.log('i-- =', i--)
+// console.log('i=', i)
 
 
 
 let a = 50;
 let b = 5;
-console.log('The value of a+b is', a+b);
-console.log('The value of a-b is', a-b);
-console.log('The value of a*b is', a*b);
-console.log('The value of a/b is', a/b);
+// console.log('The value of a+b is', a+b);
+// console.log('The value of a-b is', a-b);
+// console.log('The value of a*b is', a*b);
+// console.log('The value of a/b is', a/b);
 
-// Assignment Operators.
+// ---------Assignment Operators.
 var c = b;
 // c += 2;
 // c -= 2;
 // c *= 2;
 // c /= 2;
- console.log(c);
+// console.log(c);
 
-//Comparison Operators.
+//---------Comparison Operators.
 var x = 55;
 var y = 66;
-console.log(x == y);
-console.log(x === y);
-console.log(x != y);
-console.log(x >= y);
-console.log(x <= y);
-console.log(x > y);
-console.log(x < y);
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x != y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x > y);
+// console.log(x < y);
 
-//Logical Operators.
+//---------Logical Operators.
 let l=5;
 let m=6;
-console.log(l<m && l==5)
-console.log(l>m || l==5)
-console.log(!false)
-console.log(!true)
+// console.log(l<m && l==5)
+// console.log(l>m || l==5)
+// console.log(!false)
+// console.log(!true)
+
+// --------Conditional Operators.
