0dbafedf40e2b0fb4cdb4f1d22722e817ceecfe7
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-19 23:47:48 +0530

    Update PracticeSet2.js

--- PracticeSet2.js
+++ PracticeSet2.js
@@ -1,7 +1,7 @@
 /*
 Q1. Use logical operator to find wether the age of x person lies between 10 and 20?
 */
-let age = 50;
+let age = 15;
 if(age>10 && age<20){
     console.log('Your age lies between 1o and 20')
 }
