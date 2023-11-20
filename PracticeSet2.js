951cd8500d71ff36d2992883865d3c159eb5fa7e
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-20 23:30:02 +0530

    Update PracticeSet2.js

--- PracticeSet2.js
+++ PracticeSet2.js
@@ -29,3 +29,10 @@ switch (froutPrice){
 /*
 Q3. Write a JavaScript Program to find whether a number is divisible by 2 and 3?
 */
+let num = 22;
+if (num % 2 == 0 && num % 3 == 0){
+    console.log('Your number is divisible by 2 and 3')
+}
+else{
+    console.log('Your number is not divisible by 2 and 3')
+}
