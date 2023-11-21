b86d36cbc160ad0299d672f2fbacdf480055aeb0
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-21 18:48:22 +0530

    Update 03_Loops.js

--- 03_Loops.js
+++ 03_Loops.js
@@ -1,3 +1,13 @@
+/*
 for (let i = 0; i<5; i++){
     console.log(i)
 }
+*/
+
+// Program to add first n natural numbers.
+let sum = 0;
+let n = 3;
+for (let i=0; i<n; i++){
+    sum += (i+1)
+}
+console.log('Sum of first '+ n +' Natural numbers is '+ sum)
