df006dcdff6ea0413b5ee882a96099ecf126ec29
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-21 19:03:13 +0530

    Update 03_Loops.js

--- 03_Loops.js
+++ 03_Loops.js
@@ -4,7 +4,7 @@ for (let i = 0; i<5; i++){
 }
 */
 
-// Program to add first n natural numbers.
+/* Program to add first n natural numbers.
 let sum = 0;
 let n = 4;
 for (let i=0; i<n; i++){
@@ -12,4 +12,15 @@ for (let i=0; i<n; i++){
     console.log((i+1), "+")
 }
 console.log('Sum of first '+ n +' Natural numbers is '+ sum)
+*/
 
+let obj = {
+    Jitu: 90,
+    Shubham: 85,
+    Vikash: 80,
+    Ramesh: 70,
+    Harish: 60
+}
+for(let a in obj){
+    console.log('Marks of ' + a + ' is ' + obj[a])
+}
