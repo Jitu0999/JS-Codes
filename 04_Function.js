df891aa4f0b1cbc949c4d861a89c27e5d1a1ba85
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-23 21:18:51 +0530

    Update 04_Function.js

--- 04_Function.js
+++ 04_Function.js
@@ -1,3 +1,10 @@
+const sum = (p, q)=>{
+    return p + q
+}
+console.log(sum(5, 4))
+
+
+/*
 function onePlusAvg(x, y){
     console.log('Done')
     return 1+(x+y)/2
@@ -9,3 +16,4 @@ let c = 3;
 console.log("Average of a and b is ", onePlusAvg(a, b))
 console.log("Average of b and c is ", onePlusAvg(b, c))
 console.log("Average of a and c is ", onePlusAvg(a, c))
+*/
