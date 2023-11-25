2d072867dc737fa2a5931d3296ef023a59bbc0ca
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-25 23:29:09 +0530

    Update 05_String.js

--- 05_String.js
+++ 05_String.js
@@ -4,3 +4,9 @@ console.log(friend)
 console.log(name.length)
 console.log(name[0])
 console.log(name[1])
+
+// Template literals
+let boy1 = 'pramod'
+let boy2 = 'Kikhil'
+let sentence = `boy2 is a friend of boy1`
+console.log(sentence)
