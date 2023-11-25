970f84ddce45d0154b0d6dd38903d7ae30c1d477
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-25 23:39:49 +0530

    Update 05_String.js

--- 05_String.js
+++ 05_String.js
@@ -10,3 +10,8 @@ let boy1 = 'Pramod'
 let boy2 = 'Nikhil'
 let sentence = `${boy2} is a friend of ${boy1}`
 console.log(sentence)
+
+// Escape sequence characters.
+let fruit = 'Bana\'na'
+console.log(fruit)
+console.log(fruit.length)
