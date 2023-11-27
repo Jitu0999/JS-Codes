cdbeb10dbd19959bf3c8c511096720d71c31790a
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-27 16:53:05 +0530

    Update PracticeSet4.js

--- PracticeSet4.js
+++ PracticeSet4.js
@@ -4,3 +4,13 @@ Q.1 What will the following print in JavaScript. console.log("Jit\"".leigth)
 let str = "Jit\""
 console.log(str.length)// 4
 
+
+// .includes() returns true.
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const word = 'fox';
+console.log(sentence.includes(word))
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? 'is' : 'is not'
+  } in the sentence`,
+);
