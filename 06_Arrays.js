15f9f7520047919f649e67b9831a7e395e71f726
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-28 09:25:43 +0530

    Update 06_Arrays.js

--- 06_Arrays.js
+++ 06_Arrays.js
@@ -1,4 +1,9 @@
 let marks_class_12 = [81, 92, 63, 74, 85, false, "Not Present"]
 console.log(marks_class_12);
-
- 
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6])
