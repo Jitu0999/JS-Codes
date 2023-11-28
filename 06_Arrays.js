5831901a871c833b74ea7e5e1d42fb6e19f21cc0
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-28 09:40:33 +0530

    Update 06_Arrays.js

--- 06_Arrays.js
+++ 06_Arrays.js
@@ -6,5 +6,7 @@ console.log(marks_class_12[2])
 console.log(marks_class_12[3])
 console.log(marks_class_12[4])
 console.log(marks_class_12[5])
+console.log(marks_class_12[6]) // Will be undefined because index 6 does not exist.
+console.log("The length of marks class 12th is :", marks_class_12.length)
+marks_class_12[6] = 89 // Adding a new value to the array.
 console.log(marks_class_12[6])
-console.log("The length of marks class 12th is", marks_class_12.length)
