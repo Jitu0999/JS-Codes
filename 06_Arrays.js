0d1e38ebef60692871cecf1d5185c746758c3074
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-28 11:07:46 +0530

    Update 06_Arrays.js

--- 06_Arrays.js
+++ 06_Arrays.js
@@ -19,7 +19,7 @@ let b = num.toString() // b is now a string.
 console.log(b, typeof b);
 let c = num.join(" and ") // Use to add.
 console.log(c, typeof c)
-num.pop() // To use out of last element.
+num.pop() // Updates the original array and returns the pop value.
 console.log(num)
 let r = num.pop()// Returns the popped element.
 console.log(num, r)
