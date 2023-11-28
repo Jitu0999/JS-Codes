1d971c2bb7493cee0c9b99c6bd07791ab7f7ee2a
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-28 10:21:56 +0530

    Update 06_Arrays.js

--- 06_Arrays.js
+++ 06_Arrays.js
@@ -10,5 +10,5 @@ console.log(marks_class_12[6]) // Will be undefined because index 6 does not exi
 console.log("The length of marks class 12th is :", marks_class_12.length)
 marks_class_12[6] = 89 // Adding a new value to the array.
 marks_class_12[0] = 88 // Changing the value of an array.
-console.log(marks_class_12)
+console.log("Updated Arrays are : " + marks_class_12)
 console.log(typeof marks_class_12)
