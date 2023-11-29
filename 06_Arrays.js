f33588f02a1feee8e56b4734926938a4cdcc3f38
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-11-29 23:26:16 +0530

    Update 06_Arrays.js

--- 06_Arrays.js
+++ 06_Arrays.js
@@ -62,4 +62,11 @@ let arr = [330, 8, 55, 400, 50, 1, 30, 55]
 //console.log(arr)
 
 let newArr = arr.slice(3, 5)
-console.log(newArr)
+//console.log(newArr)
+
+// Looping with Array
+
+let num4 = [3, 54, 1, 2, 4]
+for (let i=0; i<num4.length; i++){
+    console.log(num4[i])
+}
