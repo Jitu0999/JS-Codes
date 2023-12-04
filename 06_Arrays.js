9fe341d2b6313843e161f0144f9363a43d58790c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-04 22:09:22 +0530

    Update 06_Arrays.js

--- 06_Arrays.js
+++ 06_Arrays.js
@@ -118,9 +118,18 @@ let a = arr1.map((value, index, array)=>{
 console.log(a)
 */
 
-// Array filter method
+/* Array filter method
 let arr2 = [45, 23, 21, 0, 3, 5]
 let a2 = arr2.filter((k) =>{
     return k < 10
 })
 console.log(a2)
+*/
+
+// Array reduce method
+let arr3 = [1, 2, 3, 4, 5]
+const reduce_func = (h1, h2) =>{
+    return h1 + h2
+}
+let newarr3 = arr3.reduce(reduce_func)
+console.log(newarr3)
