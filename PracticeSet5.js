c7390b47397a0d6b1959cb7543047b39ca8b0a1f
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-05 20:45:50 +0530

    Update PracticeSet5.js

---
+++ PracticeSet5.js
@@ -0,0 +1,21 @@
+/*
+Q.1 Create an array of numbers and take input from the user to add numbers to this array?
+*/
+
+
+/*
+Q.2 Keep adding numbers to the array in 1 until 0 is added to the array?
+*/
+
+/*
+Q.3 Filter for numbers divisible by 10 from a given array? 
+*/
+
+/*
+Q.4 Create an array of squire of given numbers? 
+*/
+
+/*
+Q.5 Use reduce to calculate fictorial of a given number from an array of first n natural numbers(n being the number where factorial ruls to be calculated) 
+*/
+
