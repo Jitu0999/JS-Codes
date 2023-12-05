3290b91ab49c37b45cf90ff9d3d6b88b5599e8aa
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-05 20:56:59 +0530

    Update PracticeSet5.js

--- PracticeSet5.js
+++ PracticeSet5.js
@@ -10,6 +10,15 @@ console.log(arr)
 
 /*
 Q.2 Keep adding numbers to the array in 1 until 0 is added to the array?
+let arr = [1, 2, 3, 4, 5, 6]
+let a;
+do {
+    a = prompt("Enter a number")
+    a = Number.parseInt(a)
+    arr.push(a)
+}
+while (a != 0);
+console.log(arr)
 */
 
 /*
