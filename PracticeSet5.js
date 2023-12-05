a8c094963848d26c67bc84c8cb46659dc9e2a0b5
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-05 21:04:49 +0530

    Update PracticeSet5.js

--- PracticeSet5.js
+++ PracticeSet5.js
@@ -23,17 +23,19 @@ console.log(arr)
 
 /*
 Q.3 Filter for numbers divisible by 10 from a given array? 
-*/
+
 let arr = [1, 2, 30, 4, 50, 6, 70, 8, 90]
 let n = arr.filter((x) =>{
     return x%10 == 0
 })
 console.log(n)
-
+*/
 
 /*
 Q.4 Create an array of squire of given numbers? 
 */
+
+
 
 /*
 Q.5 Use reduce to calculate fictorial of a given number from an array of first n natural numbers(n being the number where factorial ruls to be calculated) 
