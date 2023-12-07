22a199936fed15fde20d1549122983ef25139a4c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-07 23:50:40 +0530

    Update Tag.js

--- Tag.js
+++ Tag.js
@@ -2,7 +2,12 @@
 1. Using between <script></script> tag in index.html file.
 2. Seprate file with .js extension.
 
+Advantage of using script tag.
+1. Separation of concerns.
+2. Browser caching..
 
+
+*/
 console.log('Hello')
 let a = 4;
 let b = 6;
