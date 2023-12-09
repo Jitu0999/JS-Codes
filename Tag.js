0c940095f7abea924942a905b198ab065a4f0cfd
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-09 22:22:20 +0530

    Update Tag.js

--- Tag.js
+++ Tag.js
@@ -33,3 +33,11 @@ for(let i=0;i<5;i++){
     console.log(233)
 }
 console.timeEnd('forLoop')
+
+console.time('whileLoop')
+let i=0;
+while(i<5){
+    console.log(233)
+    i++;
+}
+console.timeEnd('whileLoop')
