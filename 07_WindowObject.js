eec327c95e77fe7eb649c7171cc4aa7c60e6cb5b
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-11 20:20:49 +0530

    Update 07_WindowObject.js

--- 07_WindowObject.js
+++ 07_WindowObject.js
@@ -4,4 +4,5 @@ window.console.log(window)
 window.alert('Hi..')
 console.log(document.body)
 document.body.style.background = 'yellow'
-
+// BOM Browser Object Model.
+location.href = "https://google.com"
