9fb0277ee0dbff20c7d3624f9aa81a38e55cba0c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-12 22:20:54 +0530

    Update PracticeSet6.js

--- PracticeSet6.js
+++ PracticeSet6.js
@@ -17,7 +17,7 @@ else {
 
 /*
 Q.2 In Q1 use confirm to ask the user if he wants to see the prompt again ?
-*/
+
 
 //let age = prompt('Enter your age ')
 //age = Number.parseInt(age)
@@ -28,6 +28,31 @@ const canDrive = (age)=>{
 while(runAgain){
     let age = prompt('Enter your age ')
     age = Number.parseInt(age)
+
+if (canDrive(age)){
+    alert('Yes you can drive')
+}
+else {
+    alert('You cannot drive.')
+}
+runAgain = confirm('Do you want to play again??')
+}
+*/
+
+/*
+Q.3 In the privious Q2 use console.error to log the error if the age enterd is negative?
+*/
+let runAgain = true;
+const canDrive = (age)=>{
+    return age>=18?true:false
+}
+while(runAgain){
+    let age = prompt('Enter your age ')
+    age = Number.parseInt(age)
+    if (age < 0) {
+        console.error('Please enter a valid age.');
+        break;
+    }
 
 if (canDrive(age)){
     alert('Yes you can drive')
