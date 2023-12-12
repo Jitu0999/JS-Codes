2d317482db3ccba56791b22698dd76566499e3bb
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-12 21:58:27 +0530

    Update PracticeSet6.js

--- PracticeSet6.js
+++ PracticeSet6.js
@@ -1,6 +1,6 @@
 /*
-Q.4 Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive?
-*/
+Q.1 Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive?
+
 let age = prompt('Enter your age ')
 age = Number.parseInt(age)
 const canDrive = (age)=>{
@@ -12,3 +12,10 @@ if (canDrive(age)){
 else {
     alert('You cannot drive.')
 }
+
+*/
+
+/*
+Q.2 In Q1 use confirm to ask the user if he wants to see the prompt again ?
+*/
+
