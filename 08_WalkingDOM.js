59985c71035434c3e10e88262ecfe24bd8afef4c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-14 18:50:25 +0530

    Update 08_WalkingDOM.js

--- 08_WalkingDOM.js
+++ 08_WalkingDOM.js
@@ -2,5 +2,10 @@
 DOM tree refers the HTML page where all the nodes are objects. There can be 3 main types of nodes in the DOM tree-
 1. Text nodes
 2. Element nodes
-3. Comment nodes 
+3. Comment nodes
+
+In an HTML page, <html> is at the root and <head> and <body> are its children, etc.
+A text 
+
+
 */
