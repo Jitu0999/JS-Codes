c9073b4d58b95498e3078b1f20a3198549415d95
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-15 23:44:03 +0530

    Update 09_Children.js

--- 09_Children.js
+++ 09_Children.js
@@ -4,6 +4,8 @@ Direct as well as display nested elements of an element are called its children.
 
 Child nodes- Elements that are direct children,For example head and body are children of <html>
 
-Descendant nodes - all
+Descendant nodes - all nested elements, children their children and so on...
+
+
 
 */
