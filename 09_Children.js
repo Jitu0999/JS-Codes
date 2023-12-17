d4a076701e49d3ba18151216a0cba598c9353177
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-17 19:28:13 +0530

    Update 09_Children.js

--- 09_Children.js
+++ 09_Children.js
@@ -26,4 +26,7 @@ Notes on DOM Collections :-
 
 console.log(document.body.firstChild) // atext
 console.log(document.body.lastChild) // script
-console.log(document.body.childNodes) // nodeList(8) [text, h2, text, span, text, div, text, script]
+//console.log(document.body.childNodes) // nodeList(8) [text, h2, text, span, text, div, text, script]
+let arr = Array.from(document.body.childNodes)
+//console.log(arr)
+
