f5c6dd50c50c6baf697387b3f36bbeb085bdedf2
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-18 18:58:25 +0530

    Update 10_Parents&Siblings.js

--- 10_Parents&Siblings.js
+++ 10_Parents&Siblings.js
@@ -5,6 +5,8 @@ They are read-only
 They are live clem.childNodes variable(refrence) will be automatically update if childNodes of elem is changed.
 They are iterable using for of loop.
 
+Siblings and the parent:-
+Siblings are nodes that are children of the same parent.
 
 
 */
