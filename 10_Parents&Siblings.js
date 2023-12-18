757a86e122f6a7670ba21f848972881e3d14e93c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-18 18:55:54 +0530

    Update 10_Parents&Siblings.js

--- 10_Parents&Siblings.js
+++ 10_Parents&Siblings.js
@@ -3,6 +3,8 @@
 Notes on DOM Collections :-
 They are read-only
 They are live clem.childNodes variable(refrence) will be automatically update if childNodes of elem is changed.
+They are iterable using for of loop.
+
 
 
 */
