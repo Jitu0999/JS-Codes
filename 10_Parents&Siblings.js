5b8d84a3a2d512dbd2aed9e9b7b11fdb6a825665
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-19 22:39:44 +0530

    Update 10_Parents&Siblings.js

--- 10_Parents&Siblings.js
+++ 10_Parents&Siblings.js
@@ -1,11 +1,11 @@
 
 /*
-Notes on DOM Collections :-
+Notes on DOM Collections :-----
 They are read-only
 They are live clem.childNodes variable(refrence) will be automatically update if childNodes of elem is changed.
 They are iterable using for of loop.
 
-Siblings and the parent:-
+Siblings and the parent :-----
 Siblings are nodes that are children of the same parent.
 
 For example:-
@@ -15,10 +15,16 @@ Siblings have same parent. in the above example its html.
 <body> is said to be the "next" or "right" sibling of <head>
 <head> is said to be the "privious" or "left" sibling of <body
 
+The next sibling is in nextSibling property, and the privious one in previousSibling.
+The parent is available as parentNode.
 
-
+alert(document.documentElement.parentNode); // document
+alert(document.documentElement.parentElement); // null
 
 
 
 
 */
+
+
+
