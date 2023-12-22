31b38cfa865438819129aff5d5791019734d0107
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-22 19:02:42 +0530

    Update 12_TableBasedNavigation.js

--- 12_TableBasedNavigation.js
+++ 12_TableBasedNavigation.js
@@ -8,7 +8,11 @@ table.caption ----> refrence to <caption>
 table.tHead ----> reftence to <thead>
 table.tFoot ----> refrence to <tfoot>
 table.tBodies ----> collection to <tbody> elements
-tbody.rows ----> collection to <tr> inside
+tbody.rows ----> collection of <tr> inside
 
+tr.cells ----> collection of td and th
+tr.sectionRowIndex ----> Index of tr inside enclosing element.
+tr.rowIndex ----> Row number starting from 0
 
+td.cellIndex ----> No. of cells inside enclosing <tr>
 */
