fab8dbb3b0d52645afe24f4f84fb4db0ff297824
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-22 18:47:46 +0530

    Update 12_TableBasedNavigation.js

--- 12_TableBasedNavigation.js
+++ 12_TableBasedNavigation.js
@@ -3,6 +3,7 @@ Table links :-
 Certen DOM elements may provide additional properties specifie to their type for convenience.
 Table element supports the following properties :-
 
+table.rows ----> Collection of tr elements.
 
 
 */
