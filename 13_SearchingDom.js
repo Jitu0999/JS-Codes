d8b3da50652a61a7814745d0cb9d03aaa708c6ff
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-27 21:44:25 +0530

    Update 13_SearchingDom.js

--- 13_SearchingDom.js
+++ 13_SearchingDom.js
@@ -1,6 +1,8 @@
 /*
 Searching the DOM:-
 DOM navigation properties are helpful when the elements are close to each other.
+If they are not close to each other, we have some more methods to search the DOM.
+
 
 
 */
