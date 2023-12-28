009192cba0d87630f9b3a23750c06babb9f74901
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2023-12-28 22:38:11 +0530

    Update 13_SearchingDom.js

--- 13_SearchingDom.js
+++ 13_SearchingDom.js
@@ -5,6 +5,7 @@ If they are not close to each other, we have some more methods to search the DOM
 
 document.getElementById -
 This method is used to get the element with a given id attribute.
+let span = document.getElementById('span')
 
 
 
