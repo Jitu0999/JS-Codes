af501d55fca3f1881f3118926637b24e888b948c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-01 19:03:15 +0530

    Update 14_MatchesClosest&Contains.js

---
+++ 14_MatchesClosest&Contains.js
@@ -0,0 +1,10 @@
+/*
+Matches, Closest and Contains methods:-
+There are three important methods to search the dom.
+1. elem.matches(css) -> To check if element matches the given css selector.
+2. elem.closet(css) -> To look for the nearest ancestor that matches the given css selector. The elem itself is also checked.
+3. elemA.contains(elemB) -> Returns true if elemB is inside elemA (a descendant of elemA) or when elemA == elemB.
+
+
+*/
+
