ad8f4853df4a3bdf06c9e987a9362821d8ba02c1
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-01 18:53:45 +0530

    Update 13_SearchingDom.js

--- 13_SearchingDom.js
+++ 13_SearchingDom.js
@@ -30,7 +30,8 @@ Matches, Closest and Contains methods:-
 There are three important methods to search the dom.
 1. elem.matches(css) -> To check if element matches the given css selector.
 2. elem.closet(css) -> To look for the nearest ancestor that matches the given css selector. The elem itself is also checked.
-3. 
+3. elemA.contains(elemB) -> Returns true if elemB is inside elemA (a descendant of elemA) or when elemA == elemB.
+ 
 
 
 
