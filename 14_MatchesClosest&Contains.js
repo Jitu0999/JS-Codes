561b86c9cfa8b8624d66bd8f61a9901cae1d57fc
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-02 23:53:30 +0530

    Update 14_MatchesClosest&Contains.js

--- 14_MatchesClosest&Contains.js
+++ 14_MatchesClosest&Contains.js
@@ -14,6 +14,8 @@ console.log(id1.matches(".box"))
 console.log(sp1.closest(".box"))
 console.log(sp1.closest("#sp1"))
 console.log(id1.contains("sp1"))
+console.log(sp1.contains("sp1"))
+console.log(sp1.contains("id1"))
 
 
 
