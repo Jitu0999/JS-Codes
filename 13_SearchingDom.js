ab7c273c17cbe4de0d66d8b5806d04403cb8908e
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-02 23:27:51 +0530

    Update 13_SearchingDom.js

--- 13_SearchingDom.js
+++ 13_SearchingDom.js
@@ -26,14 +26,6 @@ Returns elements that have the given css class
 6. document.getElementsByName :-
 Searches elements by name attribute.
 
-Matches, Closest and Contains methods:-
-There are three important methods to search the dom.
-1. elem.matches(css) -> To check if element matches the given css selector.
-2. elem.closet(css) -> To look for the nearest ancestor that matches the given css selector. The elem itself is also checked.
-3. elemA.contains(elemB) -> Returns true if elemB is inside elemA (a descendant of elemA) or when elemA == elemB.
- 
-
-
 
 */
 
