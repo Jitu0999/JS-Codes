e7d2ae5cb82031bc45dcf69b847d6cd21136ab5c
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-05 23:08:51 +0530

    Update PracticeSet7.js

--- PracticeSet7.js
+++ PracticeSet7.js
@@ -6,4 +6,11 @@ document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red"
 
 /*
 Q.2 Create a table without tbody. Now use "View page source" button to check when ther it has a tbody or not?
+Ans-  NO.
 */
+
+/*
+Create an element with 3 children now change the color of first and last element to green?
+*/
+
+
