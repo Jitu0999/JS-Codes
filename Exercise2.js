3cf41d80ce083e7abea42ba7aeaf388bd43b19cc
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-07 23:00:21 +0530

    Update Exercise2.js

---
+++ Exercise2.js
@@ -0,0 +1,6 @@
+/*
+Create Snakem Water and Gun game in JavaScript.
+1. The game should ask you to enter S, W, or G. The computer should be able to randomly generate S, W or G and declare win or loss using alert.
+2. 
+
+*/
