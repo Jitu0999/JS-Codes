dcd3cd1b9f9eb092579c9969dd06da8a354efc57
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-09 23:46:58 +0530

    Update Exercise3.js

--- Exercise3.js
+++ Exercise3.js
@@ -2,6 +2,7 @@
 Tell me a joke
 
 elem.innerHtml is used to populate a div with Html.
+Search online about this method and create a website with a div tag contaning a random Joke given an array of Jokes.
   
 
 */
