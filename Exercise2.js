74faf4ccbf3fb5b2d8f65dc2ddf3721e6536cd79
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-09 23:02:44 +0530

    Update Exercise2.js

--- Exercise2.js
+++ Exercise2.js
@@ -1,16 +1,16 @@
 /*
-Create Snakem Water and Gun game in JavaScript.
+Create Snake Water and Gun game in JavaScript.
 1. The game should ask you to enter S, W, or G. The computer should be able to randomly generate S, W or G and declare win or loss using alert.
 2. Use alert, prompt and confirm where ever required.
 */
 
 let user = prompt("Enter S, W or G")
-let cpuI = Math.floor(Math.random() * 3);
+let cpuI = Math.floor(Math.random()*3);
 let cpu = ["S", "W", "G"][cpuI]
 
-const match = (cpu, user) =>{
+const match = (cpu, user)=>{
     if(cpu === user){
-        return "Nobody. Match is tied."
+        return "Nobody."
     }
     else if(cpu === "S" && user === "W"){
         return "cpu"
@@ -24,12 +24,9 @@ const match = (cpu, user) =>{
     else if(cpu === "G" && user === "S"){
         return "cpu"
     }
-    else if(cpu === "W" && user === "S"){
-        return "user"
-    }
     else if(cpu === "W" && user === "G"){
         return "cpu"
     }
 }
 let result = match(cpu, user)
-document.write(`CPU:${cpu} and User:${user} \n The winner is : ${result}`)
+document.write(`CPU : ${cpu} <br> User : ${user} <br> The winner is : ${result}`)
