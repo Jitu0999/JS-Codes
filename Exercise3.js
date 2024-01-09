ae82a1c3d664e3444438acd83cb1891c25c498a9
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-09 23:56:58 +0530

    Update Exercise3.js

--- Exercise3.js
+++ Exercise3.js
@@ -5,6 +5,6 @@ elem.innerHtml is used to populate a div with Html.
 Search online about this method and create a website with a div tag contaning a random Joke given an array of Jokes.
 Use Math.random and fatch jokes from the internet (Use any website to create the arry)
 Your website should the random joke on every reloade.
-
+Min length of your jokes array should be 10.
 
 */
