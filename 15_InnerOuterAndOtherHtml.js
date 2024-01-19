05d6901dd582b8901ac8c0bd053768f273a92fa3
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-19 19:08:36 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -7,6 +7,7 @@ console.dir -> Shows the element as an boject with its properties.
 
 Tag Name / node Name :-
 Used to read tag name of an element.
+tagName -> Only exists for Element Nodes. 
 nodeName -> Defined for any node (text, comment etc.) 
 
 
