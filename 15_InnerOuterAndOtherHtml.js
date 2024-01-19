9079534d860ebb410b056a8acfb8f52aff9bf832
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-19 18:56:16 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -6,7 +6,8 @@ console.log -> Shows the element Dom tree
 console.dir -> Shows the element as an boject with its properties.
 
 Tag Name / node Name :-
-
+Used to read tag name of an element.
+ 
 
 
 */
