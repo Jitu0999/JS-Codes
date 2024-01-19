dacd6710efb57e4061d846eb30ae6118559a5fb2
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-19 19:20:36 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -16,6 +16,9 @@ The innerHTML property allows to get the HTML inside the element as a string.(va
 The Outer HTML property contains the full HTML. (inner HTML + the element itself.)
 Inner HTML is valid only for element nodes. For other node types we can use node value or data.
 
+Text Content :-
+
+
 
 
 */
