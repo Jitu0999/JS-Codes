84105d3105c439b11e9923ef80a2736dc342c385
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-19 19:13:38 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -11,6 +11,9 @@ tagName -> Only exists for Element Nodes.
 nodeName -> Defined for any node (text, comment etc.) 
 
 Inner HTML and Outer HTML :-
+The innerHTML property allows to get the HTML inside the element as a string.(valid for element nodes only)
+
+
 
 
 */
