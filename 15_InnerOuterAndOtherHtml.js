7d72fa5ff90ab3472da1e16bcc6e1230350e2734
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-20 21:00:17 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -19,7 +19,7 @@ Inner HTML is valid only for element nodes. For other node types we can use node
 Text Content :-
 Provides access to the text inside the element. Only text, minus all tags.
 
-The Hidden peoperty.
+The Hidden peoperty:-
 The 'Hidden' attribut and the DOM property specifies whether the element is visible or not.
 <div hidden> I Am Diddne </div>
 <div id = 'element'> I Can be Hidden</div>
