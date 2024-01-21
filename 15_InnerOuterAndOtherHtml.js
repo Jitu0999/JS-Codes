9282e30c31bf5dbb1b0666b533b2bcf28aef5a7d
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-21 22:50:29 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -31,6 +31,7 @@ Attribute Methods:-
 1. elem.hasAttribute (name) -> Method to check for existence of an attribute.
 2. elem.getAttribute (name) -> Method use to get the value of an attribute.
 3. elem.setAttribute (name, value) -> Method use to set the value of an attribute.
+4. elem.removeAttribute (name) -> Method to remove the attribute from elem.
 
 
 */
