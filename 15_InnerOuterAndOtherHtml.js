45fc0984462d0435739aaf4570c46575b9f0c823
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-23 19:45:38 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -50,7 +50,8 @@ document.body.append(div)
 Here are some more insertion methods:-
 1. node.append(e) -> Append at the end of node.
 2. node.prepend(e) -> Insert at the begning of node.
-
+3. node.before(e) -> Insert before node.
+4. 
 
 
 
