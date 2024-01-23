67605cc5cb452bf1ca8d8f78d15ddd0d8fc833cc
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-23 19:36:24 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -43,6 +43,7 @@ Its availabe as element.dataset.one
 Insertion methods:-
 We looked at some ways to insert elements in the DOM. Here is another ways-
 let div = document.createElement('div') // Create.
+div.className = "alert" // set class.
 
 
 
