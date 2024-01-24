03ec77cd2555495f4555aebccb85736d936c2c87
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-24 22:40:32 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -61,7 +61,8 @@ insertAdjacent HTML/Text/Element :-
 This method is used to insert HTML.
 The first parameter is a code word, Specifying where to insert.
 Must be one of the following---
-1. 
+1. "beforebegin" - Insert HTML immediatly before element.
+2. ""
 
 
 
