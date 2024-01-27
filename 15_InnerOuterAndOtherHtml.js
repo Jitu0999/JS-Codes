c85ca6e70e5d8b08247611f93bfdb7849e06d174
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-27 22:03:14 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -93,7 +93,9 @@ If we assign something to elem.className, it replaces the whole string of classe
 Often we want to add/remove/toggle a single class.
 1. elem.classList.add/remove("class") -> add/removes a class.
 2. elem.classList.toddle("class") -> adds the class if it does not exixt, Otherwise removes it.
-3. 
+3. elem.classList.contains("class") -> checks for the given class, returns true/fals.
+
+
 
 
 
