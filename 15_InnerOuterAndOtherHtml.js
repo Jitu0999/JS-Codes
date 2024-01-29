8d38f33b7910365638acaa00c788949716991150
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-01-29 22:38:12 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -121,7 +121,8 @@ Browser Events:-
 An event is a signal that something has append. All the DOM nodes generate such signals.
 
 Some importenet DOM events are:-
-Mouse events :- 
+Mouse events :- click, context menu(right click), mouseover/mouseout, mousedown/mouseup, mousemove.
+
 
 
 
