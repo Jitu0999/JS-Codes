71d6f5b53d4c7eef24299078ee11ab3dedc12158
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-02 22:47:43 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -154,6 +154,7 @@ elem.onclick = function (event) {
 
 event.type : Event type.
 event.currentTarget : Element that handled the event.
+event.clientX / event.clientY : Coordinates of the cursor.
 
 
 
