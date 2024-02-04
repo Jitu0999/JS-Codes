4b2130bfdd9383a5c54668e9e3d91cfc7faf0ae3
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-04 22:30:15 +0530

    Update PracticeSet8.js

--- PracticeSet8.js
+++ PracticeSet8.js
@@ -4,5 +4,5 @@ Q.1 Write a program to show different alerts when different buttons are clicked?
 */
 
 /*
-Q.2 Create a website which is capable of storing bookmarks of 
+Q.2 Create a website which is capable of storing bookmarks of yor favorite
 */
