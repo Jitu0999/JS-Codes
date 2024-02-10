f2e9833c263f8f804e9fd3905e739bf73aec6d08
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-10 20:59:54 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -163,6 +163,7 @@ console.log(x);
 let y = document.getElementsByTagName('span')[0]
 console.dir(y)
 console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
 
 
 
