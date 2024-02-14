3de73224e9c48b8bdedfd3a254207ba97eb593d8
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-14 22:13:46 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -194,7 +194,7 @@ first.style.display = "none"
 let a = first.getAttribute("class")
 console.log(a)
 console.log(first.hasAttribute("class"))
-
+console.log(first.hasAttribute("style"))
 
 
 // 2:14/13:19
