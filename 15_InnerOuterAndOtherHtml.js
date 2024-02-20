325bd1765f77e61beb616a7795ff8e4b3db7411d
Jitendra Kumar Yadav <104584739+Jitu0999@users.noreply.github.com>
2024-02-20 23:13:19 +0530

    Update 15_InnerOuterAndOtherHtml.js

--- 15_InnerOuterAndOtherHtml.js
+++ 15_InnerOuterAndOtherHtml.js
@@ -224,10 +224,10 @@ first.insertAdjacentHTML('beforebegin', '<div class="test"> Before begin</div>')
 first.insertAdjacentHTML('beforeend', '<div class="test"> Before end</div>');
 first.insertAdjacentHTML('afterbegin', '<div class="test"> After begin</div>');
 first.insertAdjacentHTML('afterend', '<div class="test"> After end</div>');
-*/
+
 // Node Removal :-
 first.remove()
-
+*/
 // ClassName and classList:-
 
 
